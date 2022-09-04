6ae915ea40ea8d8dbc98e740bcf7255df50985af
Nirmal Patel <patelnirmal17592@gmail.com>
2022-09-05 10:34:58 +1200

    navbar frames added

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from './Header.module.css'
+
+const header = () => {
+  return (
+    <div className={style.navbarContainer}>
+        <div className={style.navbarWrapper}>
+            
+        </div>
+    </div>  
+  )
+}
+
+export default header
---
+++ src/pages/InsuranceHome.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from '../components/header/Header'
+
+const InsuranceHome = () => {
+  return (
+    <Header></Header>
+  )
+}
+
+export default InsuranceHome
